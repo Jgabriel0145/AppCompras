ea18ce382b95135cec9fbe714c8416c397e25eec
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2022-11-28 09:38:12 -0300

    .

---
+++ AppCompras/AppCompras/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppCompras.Models;
+using SQLite;
+
+namespace AppCompras.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+        readonly SQLiteAsyncConnection _conexao;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+
+        }
+
+        public void Insert(Produto p)
+        {
+
+        }
+
+        public void Update(Produto p)
+        {
+
+        }
+
+        public Task<List<Produto>> Select()
+        {
+            return new List<Produto>();
+        }
+
+        public Task<Produto> GetById(int id)
+        {
+            return new Produto();
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
---
+++ AppCompras/AppCompras/Models/Produto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace AppCompras.Models
+{
+    public class Produto
+    {
+        [PrimaryKey, AutoIncrement]
+        public int id { get; set; }
+        public string nome { get; set; }
+        public double quantidade { get; set; }
+        public double preco { get; set; }
+
+
+    }
+}
