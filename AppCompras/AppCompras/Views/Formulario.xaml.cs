252751408a4500e6f91f3d1b51eac08c53578858
Jgabriel0145 <joaogabriel.silva.71066701@gmail.com>
2022-11-28 21:12:40 -0300

    listagem

--- AppCompras/AppCompras/Views/Formulario.xaml.cs
+++ AppCompras/AppCompras/Views/Formulario.xaml.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using AppCompras.Models;
+
 namespace AppCompras.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
@@ -15,6 +17,29 @@ namespace AppCompras.Views
         public Formulario()
         {
             InitializeComponent();
+        }
+
+        private async void tbSalvar_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Produto p = new Produto
+                {
+                    nome = txt_nome.Text,
+                    quantidade = Convert.ToDouble(txt_quantidade.Text),
+                    preco = Convert.ToDouble(txt_preco.Text)
+                };
+
+                await App.Database.Insert(p);
+
+                await DisplayAlert("Sucesso!","Produto cadastrado com sucesso!","OK");
+
+                await Navigation.PushAsync(new Listagem());
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Erro", ex.Message, "OK");
+            }
         }
     }
 }
--- AppCompras/AppCompras/Views/Listagem.xaml.cs
+++ AppCompras/AppCompras/Views/Listagem.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using AppCompras.Models;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +15,53 @@ namespace AppCompras.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Listagem : ContentPage
     {
+        ObservableCollection<Produto> lista_produtos = new ObservableCollection<Produto>();
+
         public Listagem()
         {
             InitializeComponent();
+
+            lst_produtos.ItemsSource = lista_produtos;
+        }
+
+        private void tbNovo_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new Formulario());
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Erro",ex.Message,"OK");
+            }
+        }
+
+        private async void tbSomar_Clicked(object sender, EventArgs e)
+        {
+            double total = lista_produtos.Sum(i => i.quantidade * i.preco);
+
+            await DisplayAlert("Total", $"O total da compra é R$ {total.ToString("F2")}", "OK");
+        }
+
+        protected override void OnAppearing()
+        {
+            if (lista_produtos.Count == 0)
+            {
+                Task.Run(async () =>
+                {
+                    List<Produto> list_temporaria = await App.Database.Select();
+
+                    foreach (Produto item in list_temporaria)
+                        lista_produtos.Add(item);
+
+                    ref_carregando.IsRefreshing = false;
+                });
+            }
+        }
+
+        private void miExcluir_Clicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
